370c85daa1937478739a6895085bfce2e7e7b3e7
Mike Lay <michael.lay.email@gmail.com>
2012-08-10 16:10:07 -0400

    Adds to previous commit
    Corrects Issue with first fun of view

--- blocks/swaggg_view/view.php
+++ blocks/swaggg_view/view.php
@@ -26,25 +26,25 @@
 		</form>
 
 	</div><!-- .swaggg-controls -->
-<?php endif ?>
 
-<div class="cf"></div>
+	<div class="cf"></div>
 
-<div class="swaggg-bar-outer cf">
-	<div class="swaggg-bar">
+	<div class="swaggg-bar-outer cf">
+		<div class="swaggg-bar">
 
-		<?php if(!$swaggg->getComputed('swaggg_percentage') && !$swaggg->getComputed('swoggg_percentage')): ?>
+			<?php if(!$swaggg->getComputed('swaggg_percentage') && !$swaggg->getComputed('swoggg_percentage')): ?>
 			<div class="swaggg-bar-begin swaggg-bar"><?php echo t('First Judgement') ?></div>
 
-		<?php else: ?>
-			<div class="swaggg-bar-swaggg swaggg-bar" style="width: <?php echo $swaggg->getComputed('swaggg_percentage') - 1?>%"><span><?php echo t('Swaggg') ?></span></div>
+			<?php else: ?>
+				<div class="swaggg-bar-swaggg swaggg-bar" style="width: <?php echo $swaggg->getComputed('swaggg_percentage') - 1?>%"><span><?php echo t('Swaggg') ?></span></div>
 
-			<div class="swaggg-bar-swoggg swaggg-bar" style="width: <?php echo $swaggg->getComputed('swoggg_percentage') - 1?>%"><span><?php echo t('Swoggg') ?></span></div>
-		<?php endif ?>
+				<div class="swaggg-bar-swoggg swaggg-bar" style="width: <?php echo $swaggg->getComputed('swoggg_percentage') - 1?>%"><span><?php echo t('Swoggg') ?></span></div>
+			<?php endif ?>
 
-	</div><!-- .swaggg-bar -->
-</div><!-- .swaggg-bar-outer -->
+		</div><!-- .swaggg-bar -->
+	</div><!-- .swaggg-bar-outer -->
 
-<a href="<?php echo $next_swagg_link ?>" class="next-swaggg-link cf"><?php echo t('Next Swaggg') ?></a>
+		<a href="<?php echo $next_swagg_link ?>" class="next-swaggg-link cf"><?php echo t('Next Swaggg') ?></a>
 
+<?php endif ?>
 </div><!-- .swaggg-view -->
